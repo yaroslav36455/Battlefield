cebcbbc04e936612508b16c7130b0c02f449fa71
Yaroslav Tarasenko <yaroslav36455@gmail.com>
2020-11-17 14:45:18 +0200

    Disable iteration before creating a simulation

--- src/ua/itea/view/swing/Window.java
+++ src/ua/itea/view/swing/Window.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.function.Consumer;
@@ -186,7 +185,9 @@ public class Window extends JFrame {
 			
 			@Override
 			public void keyPressed(KeyEvent e) {
-				if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+				if (engine != null
+						&& tableManager != null
+						&& e.getKeyCode() == KeyEvent.VK_SPACE) {
 					engine.iterate();
 					
 					tableManager.update();
